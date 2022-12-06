2ef4f8e29a751f6797d6ab1cb5ccb78f1bd032d8
Chris W <thunder61golf@gmail.com>
2022-12-05 23:42:40 -0600

    WIP: adds viewmodel and collection vm.

---
+++ MauiPowerlifting/ViewModels/MaxesCollectionViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace MauiPowerlifting.ViewModels
+{
+    public class MaxesCollectionViewModel : INotifyPropertyChanged
+    {
+        MaxesViewModel maxesEdit;
+        bool isEditing;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MaxesCollectionViewModel()
+        {
+            NewCommand = new Command(
+                execute: () =>
+                {
+                    maxesEdit = new MaxesViewModel();
+                    maxesEdit.PropertyChanged += OnMaxesEditPropertyChanged;
+                    IsEditing = true;
+                    RefreshCanExecutes();
+                },
+                canExecute: () =>
+                {
+                    return !IsEditing;
+                });
+
+            SubmitCommand = new Command(
+                execute: () =>
+                {
+                    maxesEdit.PropertyChanged -= OnMaxesEditPropertyChanged;
+                    Maxes.Add(MaxesEdit);
+                    MaxesEdit = null;
+                    IsEditing = false;
+                    RefreshCanExecutes();
+                },
+                canExecute: () =>
+                {
+                    return MaxesEdit != null &&
+                           MaxesEdit.Bench > 0 &&
+                           MaxesEdit.Squat > 0 &&
+                           MaxesEdit.Deadlift > 0;
+                });
+
+            CancelCommand = new Command(
+                execute: () =>
+                {
+                    MaxesEdit.PropertyChanged -= OnMaxesEditPropertyChanged;
+                    MaxesEdit = null;
+                    IsEditing = false;
+                    RefreshCanExecutes();
+                },
+                canExecute: () =>
+                {
+                    return IsEditing;
+                });
+        }
+
+        void OnMaxesEditPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            (SubmitCommand as Command).ChangeCanExecute();
+        }
+
+        void RefreshCanExecutes()
+        {
+            (NewCommand as Command).ChangeCanExecute();
+            (SubmitCommand as Command).ChangeCanExecute();
+            (CancelCommand as Command).ChangeCanExecute();
+        }
+
+        public bool IsEditing
+        {
+            private set { SetProperty(ref isEditing, value); }
+            get { return isEditing; }
+        }
+
+        public MaxesViewModel MaxesEdit
+        {
+            set { SetProperty(ref maxesEdit, value); }
+            get { return MaxesEdit; }
+        }
+
+        public ICommand NewCommand { private set; get; }
+
+        public ICommand SubmitCommand { private set; get; }
+
+        public ICommand CancelCommand { private set; get; }
+
+        public IList<MaxesViewModel> Maxes { get; } = new ObservableCollection<MaxesViewModel>();
+
+        bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Object.Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MauiPowerlifting/ViewModels/MaxesViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiPowerlifting.ViewModels
+{
+    public class MaxesViewModel : INotifyPropertyChanged
+    {
+        int squat;
+        int bench;
+        int deadlift;
+        int press;
+        int total;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Bench
+        {
+            set { SetProperty(ref bench, value); }
+            get { return bench; }
+        }
+
+        public int Squat
+        {
+            set { SetProperty(ref squat, value); }
+            get { return squat; }
+        }
+
+        public int Deadlift
+        {
+            set { SetProperty(ref deadlift, value); }
+            get { return deadlift; }
+        }
+        public int Press
+        {
+            set { SetProperty(ref press, value); }
+            get { return press; }
+        }
+
+        bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Object.Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
